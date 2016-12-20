753434a4c55b5a10f0959649e46d2cab5f8bb7d3
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-12-20 14:35:46 +0100

    Cleanup

--- scripts/dependants.sh
+++ scripts/dependants.sh
@@ -30,14 +30,15 @@ while IFS= read -r -d '' freyFile; do
   infraDir="$(dirname "${freyFile}")"
   echo "--> Processing '${infraDir}'..."
   pushd ${infraDir} > /dev/null
-    [[ -z $(git status -s) ]] || (echo "Aborting due to dirty git index at '${infraDir}'."; git diff |cat; git status ; exit 1)
+    [[ -z $(git status -s) ]] || (git diff |cat; git status ; echo "Aborting due to dirty git index at '${infraDir}'."; exit 1)
+    # git reset --hard
     git checkout master
     git pull
     git checkout -B "frey-v${version}"
     # git pull origin "frey-v${version}" || true
     npm unlink frey || true
     if grep -q 'FREY_VERSION' ./Makefile; then
-      ${__root}/node_modules/.bin/replace "^FREY_VERSION\s*:?=.*$" "FREY_VERSION := ${version}" ./Makefile
+      "${__root}/node_modules/.bin/replace" "^FREY_VERSION\s*:?=.*$" "FREY_VERSION := ${version}" ./Makefile
       make frey
     elif [ -f ./package.json ]; then
       yarn upgrade frey@${version} || (yarn && yarn add --dev frey@${version}) || npm install --save-dev frey@${version}
@@ -50,12 +51,21 @@ while IFS= read -r -d '' freyFile; do
       exit 1
     fi
 
-    ${__root}/node_modules/.bin/replace "/v(\d+.\d+.\d+)" '/$1' "${freyFile}"
-    ${__root}/node_modules/.bin/replace "/apt/(\d+.\d+.\d+)" 'apt/1.3.0' "${freyFile}"
+    # Strip the `v` prefix that is now legacy:
+    "${__root}/node_modules/.bin/replace" "/v(\d+.\d+.\d+)" '/$1' "${freyFile}"
+
+    # Upgrade roles to their latest versions
+    while IFS= read -r -d '' roleDir; do
+      pushd "${roleDir}"
+        role="$(basename "${roleDir}")"
+        latestRoleVersion=$(ls |egrep -v '^v' |sort -nr |head -n1)
+        "${__root}/node_modules/.bin/replace" "/${role}/(\d+.\d+.\d+)" "/${role}/${latestRoleVersion}" "${freyFile}" || true
+      popd
+    done < <(find "${__root}/roles" -maxdepth 1 -type d -print0) || true
 
     git add --all .
     git commit -m "Upgrade Frey to v${version} /cc @tersmitten" || true
-    # git push
-    git push -f origin "frey-v${version}"
+    git push
+    # git push -f origin "frey-v${version}"
   popd > /dev/null
-done <   <(find "${CODE_DIR}" -maxdepth 3 -name Freyfile.hcl -print0)
+done < <(find "${CODE_DIR}" -maxdepth 3 -name Freyfile.hcl -print0)
